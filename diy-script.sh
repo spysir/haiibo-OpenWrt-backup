0a2279e486021fadd28c25546fcd1dca6d44c177
spysir <59646715+spysir@users.noreply.github.com>
2023-12-07 08:15:44 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -8,13 +8,13 @@
 #===============================================
 
 # 修改默认IP
-# sed -i 's/192.168.1.1/192.168.11.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.2/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
-# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # TTYD 自动登录
-# sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包
 rm -rf feeds/packages/net/mosdns
